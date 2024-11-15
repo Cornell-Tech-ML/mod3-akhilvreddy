b46e84e8dd519a799ac12de4ea022a6f40bbd158
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2024-11-15 18:26:15 -0500

    added 3.5 training functionality

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,8 +29,9 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        h = self.layer1.forward(x).relu()
+        h = self.layer2.forward(h).relu()
+        return self.layer3.forward(h).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -44,7 +45,7 @@ class Linear(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        raise (x @ self.weights.value) + self.bias.value
 
 
 class FastTrain:
